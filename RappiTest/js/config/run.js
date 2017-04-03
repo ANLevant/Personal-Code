57f5bdb4f9f3c9b7cc82296a32982481c385eb6f
Levant Alejandro <Levant Alejandro>
2017-04-02 21:35:01 -0500

    e suben cambios hasta fecha de prueb re Rapp

---
+++ RappiTest/js/config/run.js
@@ -0,0 +1,3 @@
+angular.module('RappiTest').run(function(){
+
+});
---
+++ RappiTest/js/controller/RappiController.js
@@ -0,0 +1,36 @@
+angular.module('RappiTest').controller('RappiController',function(){
+
+  var that  = this;
+  this.testCases = 0;
+
+  this.getTestCases = function(){
+    return new Array(that.testCases);
+  };
+
+  this.instantiate = function(){
+      that.threeDMatrix = [];
+      that.threeDMatrix[0] = [];
+      that.threeDMatrix[0][0] = [];
+  };
+
+  this.updateXYZ = function(x, y, z, value){
+    that.threeDMatrix[x][y][z] = value;
+  };
+
+  this.query = function(startX, startY, startZ, endX, endY, endZ){
+
+    var sum = 0;
+
+    for(startX; startX -1 < endX; startX++){
+      for(startY; startY - 1 < endY; startY++){
+        for(startZ; startZ -1 < endZ; startZ++){
+          sum += that.threeDMatrix[startX][startY][startZ];
+        }
+      }
+    }
+
+    return sum;
+
+  };
+
+});
