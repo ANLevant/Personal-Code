f6a18258992d02b243fe078b5b09e51a39b9728c
Levant Alejandro <Levant Alejandro>
2017-04-04 22:29:06 -0500

    Se suben cambios de cálculo de movimientos, repeticion de entradas y campos de salida

--- RappiTest/js/controller/RappiController.js
+++ RappiTest/js/controller/RappiController.js
@@ -3,11 +3,105 @@ angular.module('RappiTest').controller('RappiController',function($scope){
   var that  = this;
   this.testCases = 0;
   this.T  = {'testCases':[]};
-  this.N = 0;
-  this.M = {'moves':[]};
+  this.N = [];
+  this.M = [];
+  this.output = [];
+  this.threeDMatrix = [];
 
+  this.solve = function(){
+    that.output = [];
+    var i = 0;
+    var j = 0;
+
+    for(i = 0; i < this.testCases; i++){
+      var matrix = that.threeDMatrix[i];
+      var movement = that.M[i];
+
+      for(j = 0; j < movement.length; j++){
+        var input = movement[j].input;
+        if(movement[j].type === 'QUERY'){
+          var value = that.query(i, input[0] - 1 , input[1] - 1, input[2] - 1, input[3] - 1, input[4] - 1, input[5] - 1);
+          that.output.push('t_'+i+'m_'+j+': '+movement[j].type+' '+input+'\n'+value);
+        }
+        else if(movement[j].type === 'UPDATE'){
+          that.update(i, input[0] - 1, input[1] - 1, input[2] - 1, input[3]);
+        }
+
+      }
+
+    }
+
+  };
+
+  /**Method that validates the inputed value
+   * @param testCaseNumber test case number >= 0
+   * @param movementNumber movement number >= 0
+  */
+  this.validateMovement = function(testCaseNumber, movementNumber){
+    var movement = that.M[testCaseNumber][movementNumber].movement.split(' ');
+    var i =1;
+    var checkedInput = 0;
+    that.M[testCaseNumber][movementNumber].input = [];
+    that.M[testCaseNumber][movementNumber].type = movement[0];
+
+    if(movement[0] === 'UPDATE' && movement.length === 5){
+
+      for(i = 1; i < movement.length; i++){
+        checkedInput = parseInt(movement[i]);
+        var test1 = angular.isNumber(checkedInput);
+
+        if(i !== 4 && angular.isNumber(checkedInput) &&  checkedInput > 0 && checkedInput <= that.N[testCaseNumber]){
+          that.M[testCaseNumber][movementNumber].input.push(checkedInput);
+        }
+        else if(i === 4 && angular.isNumber(checkedInput) &&  checkedInput >= -Math.pow(10, 9) && checkedInput <= Math.pow(10, 9)){
+          that.M[testCaseNumber][movementNumber].input.push(checkedInput);
+        }
+        else{
+          that.M[testCaseNumber][movementNumber].isValid = false;
+          return false;
+        }
+      }
+
+      that.M[testCaseNumber][movementNumber].isValid = true;
+      return true;
+    }
+    else if(movement[0] === 'QUERY' && movement.length === 7){
+      for(i = 1; i < movement.length; i++){
+        checkedInput = parseInt(movement[i]);
+        if(angular.isNumber(checkedInput) && checkedInput > 0 && checkedInput <= that.N[testCaseNumber]){
+          that.M[testCaseNumber][movementNumber].input.push(checkedInput);
+        }
+        else{
+          that.M[testCaseNumber][movementNumber].isValid = false;
+          return false;
+        }
+      }
+
+      if(that.M[testCaseNumber][movementNumber].input[0] <= that.M[testCaseNumber][movementNumber].input[3] &&
+        that.M[testCaseNumber][movementNumber].input[1] <= that.M[testCaseNumber][movementNumber].input[4] &&
+        that.M[testCaseNumber][movementNumber].input[2] <= that.M[testCaseNumber][movementNumber].input[5]){
+        that.M[testCaseNumber][movementNumber].isValid = true;
+        return true;
+      }
+      else{
+        that.M[testCaseNumber][movementNumber].isValid = false;
+        return false;
+      }
+    }
+    else{
+      that.M[testCaseNumber][movementNumber].isValid = false;
+      return false;
+    }
+  };
+
+  /*Method that calculates the objects that contain the testCases after
+  user input*
+  */
   this.calculateTestCases = function(){
     this.T  = {'testCases':[]};
+    that.M = new Array(that.testCases);
+    that.N = new Array(that.testCases);
+    this.threeDMatrix = new Array(that.testCases);
 
     var i;
 
@@ -21,24 +115,40 @@ angular.module('RappiTest').controller('RappiController',function($scope){
 
     if(testCase.length !== 2){
       that.T.testCases[$index].isValid = false;
-      $scope.form['T_'+$index].$setValidity(false);
+//      $scope.form['T_'+$index].$setValidity(false);
     }
     else if(angular.isNumber(testCase[0]) && testCase[0] >= 1 && testCase[0] <= 100){
       that.T.testCases[$index].isValid = false;
-      $scope.form['T_'+$index].$setValidity(false);
+//      $scope.form['T_'+$index].$setValidity(false);
     }
     else if(angular.isNumber(testCase[1]) && testCase[1] >= 1 && testCase[1] <= 1000){
-      that.T.testCases[$index].isValid = false;
-      $scope.form['T_'+$index].$setValidity(false);
+      if(that.T.testCases['testCase'+$index] === undefined){
+
+      }
+      else{
+        that.T.testCases['testCase'+$index].isValid = false;
+      }
+//      $scope.form['T_'+$index].$setValidity(false);
     }
     else{
       that.T.testCases[$index].isValid = true;
-      that.N = parseInt(testCase[0]);
+      that.N[$index] = parseInt(testCase[0]);
+      that.M[$index] = [];
+      var i = 0;
 
-      that.instantiate(that.N);
+      for(i = 0; i < parseInt(testCase[1]); i++){
+        that.M[$index].push({'movement':'', 'isValid':true});
+      }
 
-      $scope.form['T_'+$index].$setValidity(true);
+      that.threeDMatrix[$index] = that.instantiate(that.N[$index]);
+
+
+//      $scope.form['T_'+$index].$setValidity(true);
     }
+  };
+
+  this.getTestCaseMoves = function($parentIndex){
+    return that.M['testCase'+$parentIndex];
   };
 
   this.isValid = function(testCase){
@@ -56,40 +166,42 @@ angular.module('RappiTest').controller('RappiController',function($scope){
     var j = 0;
     var k = 0;
 
-    that.threeDMatrix = [[[]]];
+    var threeDMatrix = [];
 
     for(i = 0; i < n ; i++){
-      that.threeDMatrix[''+i]=[];
+      threeDMatrix.push([]);
 
       for(j = 0; j < n ; j++){
-        that.threeDMatrix[i].push([]);
+        threeDMatrix[i].push([]);
 
-        for(k = 0; i < k ; k++){
-          that.threeDMatrix[i][j].push([]);
+        for(k = 0; k < n ; k++){
+          threeDMatrix[i][j].push([]);
 
-          that.threeDMatrix[i][j][k] = 0;
+          threeDMatrix[i][j][k] = 0;
         }
       }
     }
+
+    return threeDMatrix;
   };
 
-  this.updateXYZ = function(x, y, z, value){
-    that.threeDMatrix[x][y][z] = value;
+  this.update = function(testCase, x, y, z, value){
+    that.threeDMatrix[testCase][x][y][z] = value;
   };
 
-  this.query = function(startX, startY, startZ, endX, endY, endZ){
+  this.query = function(testCase, startX, startY, startZ, endX, endY, endZ){
 
     var sum = 0;
 
-    for(startX; startX -1 < endX; startX++){
-      for(startY; startY - 1 < endY; startY++){
-        for(startZ; startZ -1 < endZ; startZ++){
-          sum += that.threeDMatrix[startX][startY][startZ];
+    for(startX; startX <= endX; startX++){
+      for(startY; startY <= endY; startY++){
+        for(startZ; startZ <= endZ; startZ++){
+          sum += that.threeDMatrix[testCase][startX][startY][startZ];
         }
       }
     }
 
-    return;
+    return sum;
 
 
   };
